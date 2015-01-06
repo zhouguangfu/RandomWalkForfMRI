536ec5646e2253b12e6fbf0ef1976f648f32a1da
zhouguangfu <zhougf0908@gmail.com>
2015-01-06 10:26:58 +0800

    Add Z_TOP method.

--- random_walker/multi_processs/return_full_labels_rw/rw_atlas_based_mask_multiprocess7.py
+++ random_walker/multi_processs/return_full_labels_rw/rw_atlas_based_mask_multiprocess7.py
@@ -1,11 +1,9 @@
 __author__ = 'zhouguangfu'
 
 import datetime
-import multiprocessing
 import numpy as np
 import nibabel as nib
 
-from docx import Document
 from skimage.segmentation import random_walker
 from configs import *
 
@@ -19,7 +17,7 @@ left_barin_mask = nib.load(PROB_ROI_202_SUB_FILE + PROB_LEFT_BRAIN_FILE).get_dat
 right_barin_mask = nib.load(PROB_ROI_202_SUB_FILE + PROB_RIGHT_BRAIN_FILE).get_data()
 
 def process_single_subject(subject_index):
-    global image, complete_atlas_data, left_barin_mask, right_barin_mask, TOP_RANK
+    global image, complete_atlas_data, left_barin_mask, right_barin_mask
 
     indexs =  np.load(ATLAS_TOP_DIR + str(subject_index) + '_top_sort.npy')
     region_result_RW = np.zeros((complete_atlas_data.shape[0], complete_atlas_data.shape[1], complete_atlas_data.shape[2], TOP_RANK))
@@ -35,14 +33,37 @@ def process_single_subject(subject_index):
         temp_volume[right_barin_mask == 0] = 1
         temp_volume[temp_volume > 0] = 1
         markers[temp_volume <= 0] = 3 #background
-        markers[ atlas_data == 1] = 1
-        markers[ atlas_data == 3] = 2
+
+        z_atlas_mask = np.zeros_like(atlas_data)
+        if (atlas_data == 1).sum() < Z_TOP :
+            z_atlas_mask[atlas_data == 1] = 1
+        else:
+            temp_image = image[z_atlas_mask == 1, subject_index]
+            threshold = -np.sort(-temp_image)[Z_TOP]
+            z_atlas_mask[image[..., subject_index] > threshold] = 1
+            z_atlas_mask[atlas_data != 1] = 0
+        markers[z_atlas_mask == 1] = 1
+
+        z_atlas_mask = np.zeros_like(atlas_data)
+        if (atlas_data == 3).sum() < Z_TOP :
+            z_atlas_mask[atlas_data == 3] = 1
+        else:
+            temp_image = image[z_atlas_mask == 3, subject_index]
+            threshold = -np.sort(-temp_image)[Z_TOP]
+            z_atlas_mask[image[..., subject_index] > threshold] = 1
+            z_atlas_mask[atlas_data != 3] = 0
+        markers[z_atlas_mask == 1] = 2
+
+
+        # markers[ atlas_data == 1] = 1
+        # markers[ atlas_data == 3] = 2
 
         rw_labels = random_walker(image[..., subject_index], markers, beta=10, mode='bf')
         region_result_RW[rw_labels == 1, atlas_index] = 1
         region_result_RW[rw_labels == 2, atlas_index] = 3
         region_result_RW[rw_labels == 3, atlas_index] = 5
 
+        #----------------------------------------------------------------------------------
         #process left brain
         markers = np.zeros_like(image[..., subject_index])
 
@@ -51,8 +72,29 @@ def process_single_subject(subject_index):
         temp_volume[left_barin_mask == 0] = 1
         temp_volume[temp_volume > 0] = 1
         markers[temp_volume <= 0] = 3 #background
-        markers[ atlas_data == 2] = 1
-        markers[ atlas_data == 4] = 2
+
+        z_atlas_mask = np.zeros_like(atlas_data)
+        if (atlas_data == 2).sum() < Z_TOP :
+            z_atlas_mask[atlas_data == 2] = 1
+        else:
+            temp_image = image[z_atlas_mask == 2, subject_index]
+            threshold = -np.sort(-temp_image)[Z_TOP]
+            z_atlas_mask[image[..., subject_index] > threshold] = 1
+            z_atlas_mask[atlas_data != 2] = 0
+        markers[z_atlas_mask == 1] = 1
+
+        z_atlas_mask = np.zeros_like(atlas_data)
+        if (atlas_data == 4).sum() < Z_TOP :
+            z_atlas_mask[atlas_data == 4] = 1
+        else:
+            temp_image = image[z_atlas_mask == 4, subject_index]
+            threshold = -np.sort(-temp_image)[Z_TOP]
+            z_atlas_mask[image[..., subject_index] > threshold] = 1
+            z_atlas_mask[atlas_data != 4] = 0
+        markers[z_atlas_mask == 1] = 2
+
+        # markers[ atlas_data == 2] = 1
+        # markers[ atlas_data == 4] = 2
 
         rw_labels = random_walker(image[..., subject_index], markers, beta=10, mode='bf')
         region_result_RW[rw_labels == 1, atlas_index] = 2
